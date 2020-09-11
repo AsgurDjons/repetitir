4c99dd0e20a994ccdc134a533d0a2580c4edee78
AsgurDjons <61666677+AsgurDjons@users.noreply.github.com>
2020-09-11 20:47:06 +0300

    20/9

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ function showHeader() {
     let header = document.querySelector('.header');
     setTimeout(() => {
         header.innerHTML = `Вам нужен репетитор? <br> Тогда вы зашли именно туда!!`;
-    }, 2000);
+    }, 3000);
 }
 showHeader();
 
