71f892e390790ad7c926986a251c5bb942f2f89e
AsgurDjons <61666677+AsgurDjons@users.noreply.github.com>
2020-09-12 01:49:12 +0300

    heder block add
    animation, add text, fich

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
+    // fasad site
+    const textHeader = `Вам нужен репетитор? <br> <span class="span-header">Тогда вы зашли именно туда!!</span>`,
+          textSubscr = `<p class="subscr">Математика</p><p class="subscr">Русский</p>`;
 
-function showHeader() {
-    let header = document.querySelector('.header');
-    setTimeout(() => {
-        header.innerHTML = `Вам нужен репетитор? <br> <span class="span-header">Тогда вы зашли именно туда!!</span>`;
-    }, 2000);
-}
-showHeader();
+    function showHeader(block, text, time) {
+        let blocks = document.querySelector(block);
+        setTimeout(() => {
+            blocks.innerHTML = text;
+        }, time);
+    }
+    showHeader('.header', textHeader, 2000);
+    showHeader('.wrapper', textSubscr, 3000);
 
 });
