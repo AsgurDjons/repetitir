b2c9b60621c2b15d0a954f36dcff5b92109f6e09
AsgurDjons <61666677+AsgurDjons@users.noreply.github.com>
2020-09-11 23:17:04 +0300

    1 block text

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,8 @@ window.addEventListener('DOMContentLoaded', () => {
 function showHeader() {
     let header = document.querySelector('.header');
     setTimeout(() => {
-        header.innerHTML = `Вам нужен репетитор? <br> Тогда вы зашли именно туда!!`;
-    }, 3000);
+        header.innerHTML = `Вам нужен репетитор? <br> <span class="span-header">Тогда вы зашли именно туда!!</span>`;
+    }, 2000);
 }
 showHeader();
 
