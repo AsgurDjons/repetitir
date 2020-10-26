4fc62fce25e69160ccb1fa19b8fe990ccec410a3
AsgurDjons <61666677+AsgurDjons@users.noreply.github.com>
2020-10-26 23:00:42 +0300

    send

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-    // fasad site
+// fasad site
     const textHeader = `Вам нужен репетитор? <br> <span class="span-header">Тогда вы зашли именно туда!!</span>`,
           textSubscr = `<p class="subscr">Математика</p><p class="subscr">Русский</p>`;
 
@@ -9,10 +9,16 @@ window.addEventListener('DOMContentLoaded', () => {
             blocks.innerHTML = text;
         }, time);
     }
+    setTimeout(()=> {
+        const svg = document.querySelector('#more-arrows');
+        svg.style.display = 'block';
+    }, 2500);
 
     showHeader('.header', textHeader, 1000);
     showHeader('.wrapper', textSubscr, 2000);
 
+
+// Scrol
     function showPhoto () {
         let img = document.querySelector('.my-photo');
         if (document.documentElement.clientWidth <= 500) {
